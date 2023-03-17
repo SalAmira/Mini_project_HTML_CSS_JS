662a841935c334abae9bdbba1b66f481ebefcde4
SalAmira <127787292+SalAmira@users.noreply.github.com>
2023-03-18 00:43:01 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
+    <link rel="stylesheet" href="Ajax.css">
+</head>
+<body>
+    <div class="wrapper">
+        <section class="form login">
+            <header>Realtime chat App</header>
+            <form action="#" autocomplete="off">
+                <div class="error-txt">this is an error message!</div>
+                  <div class="field input">
+                      <label for="">Email</label>
+                      <input type="text" placeholder="Enter your email">
+                      <i class="fas fa eye"></i>
+                  </div>
+                  <div class="field input">
+                      <label for="">Password</label>
+                      <input type="password" placeholder="Enter your passwoed">
+                      <i class="fa-solid fa-eye"></i>
+                  </div>
+                  <div class="field button">
+                  <input type="submit" value="Continue to chat">
+                  </div>
+                  <div class="link">Not yet signed up? <a href="Ajax.php">signup now</a></div>
+               
+            </form>
+
+        </section>
+    </div>
+    <script src="hide.js"></script>
+    <script src="login.js"></script>
+</body>
+</html>
